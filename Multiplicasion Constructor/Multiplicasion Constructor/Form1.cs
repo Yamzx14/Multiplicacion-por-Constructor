9ec914e4a2c8f3f630c7836bc370d589d11db4dc
Yamzx14 <99759853+Yamzx14@users.noreply.github.com>
2022-09-19 22:32:46 -0500

    Add files via upload

---
+++ Multiplicasion Constructor/Multiplicasion Constructor/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplicasion_Constructor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Multiplicaclass mult = new Multiplicaclass
+                (int.Parse(textBox1.Text),
+                int.Parse(textBox2.Text));
+
+            textBox3.Text = mult.Multi().ToString();
+        }
+    }
+}
---
+++ Multiplicasion Constructor/Multiplicasion Constructor/Multi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplicasion_Constructor
+{
+    internal class Multiplicaclass
+    {
+    
+        private int n1;
+        private int n2;
+
+
+        
+        public Multiplicaclass(int n1, int n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+      
+        public int Multi()
+        {
+            return (n1 * n2);
+        }
+
+    }
+}
